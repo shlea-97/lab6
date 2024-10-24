067314bf28ade4acf7f52c98664647a1d6e448dc
shlea-97 <ar.lea@ufl.edu>
2024-10-23 20:38:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 #Ariana Lea
 
-print('test,test')
+def encoder():
+    password = input()
+    #password into list of integers
+    password =[int(i) for i in password]
+    code = 0
+    coded_password=[]
+    for i in password:
+        code = i+3
+        coded_password.append(code)
+    #coded_password list into string
+    coded_password =[str(i) for i in coded_password]
+    #connect strings
+    coded_password = "".join(coded_password)
+    print(coded_password)
+    
+encoder()
